d66561c4ecc9b5db67978428942efe76391330f0
oharakogakuin <ohara@g.neec.ac.jp>
2023-04-25 16:23:29 +0900

    01_07.階層構造の構築

--- DirectXGame/3d/FbxLoader.cpp
+++ DirectXGame/3d/FbxLoader.cpp
@@ -1,5 +1,6 @@
 ﻿#include "FbxLoader.h"
 #include "DirectXCommon.h"   // 追加
+using namespace DirectX;
 
 /// <summary>
 /// 静的メンバ変数の実体
@@ -58,7 +59,79 @@ void FbxLoader::LoadModelFromFile(
 	// ファイルからロードしたFBXの情報をシーンにインポート
 	fbxImporter->Import(fbxScene);
 
+	// モデル生成
+	Model* model = new Model();
+	model->name = modelName;
 
+	// FBXノードの数を取得
+	int nodeCount = fbxScene->GetNodeCount();
+	// あらかじめ必要数分のメモリを確保することで、アドレスがずれるのを予防
+	model->nodes.reserve(nodeCount);
+
+	// ルートノードから順に解析してモデルに流し込む
+	ParseNodeRecursive(model, fbxScene->GetRootNode());
+	// FBXシーン解放
+	fbxScene->Destroy();
+
+
+
+}
+
+
+void FbxLoader::ParseNodeRecursive(Model* model, FbxNode* fbxNode, Node* parent)
+{
+	// モデルにノードを追加
+	model->nodes.emplace_back();
+	Node& node = model->nodes.back();
+	// ノード名を取得
+	node.name = fbxNode->GetName();
+	// モデルにノードを追加 (Todo)
+	// FBXノードのローカル移動情報
+	FbxDouble3 rotation = fbxNode->LclRotation.Get();
+	FbxDouble3 scaling = fbxNode->LclScaling.Get();
+	FbxDouble3 translation = fbxNode->LclTranslation.Get();
+	// 形式変換して代入
+	node.rotation = { (float)rotation[0], (float)rotation[1], (float)rotation[2], 0.0f };
+	node.scaling = { (float)scaling[0], (float)scaling[1], (float)scaling[2], 0.0f };
+	node.translation = { (float)translation[0], (float)translation[1], (float)translation[2], 1.0f };
+
+	// 回転角をDegree(度)からラジアンに変換
+	node.rotation.m128_f32[0] = XMConvertToRadians(node.rotation.m128_f32[0]);
+	node.rotation.m128_f32[1] = XMConvertToRadians(node.rotation.m128_f32[1]);
+	node.rotation.m128_f32[2] = XMConvertToRadians(node.rotation.m128_f32[2]);
+
+	// スケール、回転、平行移動行列の計算
+	XMMATRIX matScaling, matRotation, matTranslation;
+	matScaling = XMMatrixScalingFromVector(node.scaling);
+	matRotation = XMMatrixRotationRollPitchYawFromVector(node.rotation);
+	matTranslation = XMMatrixTranslationFromVector(node.translation);
+
+	// 次ページに続く
+
+	// ローカル変形行列の計算
+	node.transform = XMMatrixIdentity();
+	node.transform *= matScaling; // ワールド行列にスケーリングを反映
+	node.transform *= matRotation; // ワールド行列に回転を反映
+	node.transform *= matTranslation; // ワールド行列に平行移動を反映
+
+	// グローバル変形行列の計算 (Todo)
+	// FBXノードのメッシュ情報を解析 (Todo)
+
+	// FBXノードの情報を解析してノードに記録 (Todo)
+	// FBXノードのメッシュ情報を解析 (Todo)
+
+	//グローバル変形行列の計算
+	node.globalTransform = node.transform;
+	if (parent) {
+		node.parent = parent;
+		// 親の変形を乗算
+		node.globalTransform *= parent->globalTransform;
+	}
+
+	// 子ノードに対して再帰呼び出し
+	for (int i = 0; i < fbxNode->GetChildCount(); i++) {
+		ParseNodeRecursive(model, fbxNode->GetChild(i), &node);
+	}
 }
 
 
