127eff4911d72a451dbd442b3dcc0f0cd1a7d0dc
oharakogakuin <ohara@g.neec.ac.jp>
2023-06-28 15:26:35 +0900

    03_05.ポストエフェクト実現_完成2

--- DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
+++ DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
@@ -6,9 +6,23 @@ SamplerState smp : register(s0);
 
 float4 main(VSOutput input) : SV_TARGET
 {
-    float4 texcolor = tex.Sample(smp, input.uv);
-
-    return float4(texcolor.rgb, 1);
+// UV指定したピクセルの色をサンプリング
+    //float4 texcolor = tex.Sample(smp, input.uv);
+    //return float4(texcolor.rgb, 1);
+// アルファに1を入れて出力
+    
+    float4 texcolorTotal = 0;
+    for (int y = -1; y <= 1; y++)
+    {
+        for (int x = -1; x <= 1; x++)
+        {
+            float4 texcolor = tex.Sample(smp, input.uv + float2(x / 640.0f, y / 360.f));
+            texcolorTotal += texcolor;
+        }
+    }
+    texcolorTotal = texcolorTotal / 9;
+    return float4(texcolorTotal.rgb, 1);
+    
 }
 
 
