91d805c6adf8f09429746887566d968b6a354766
oharakogakuin <ohara@g.neec.ac.jp>
2023-06-29 15:18:33 +0900

    評価課題２

--- DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
+++ DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
@@ -12,28 +12,30 @@ float4 main(VSOutput input) : SV_TARGET
     //return float4(texcolor.rgb, 1);
 // アルファに1を入れて出力
     
-    //float4 texcolorTotal = 0;
-    //for (int y = -1; y <= 1; y++)
-    //{
-    //    for (int x = -1; x <= 1; x++)
-    //    {
-    //        float4 texcolor = tex.Sample(smp, input.uv + float2(x / 640.0f, y / 360.f));
-    //        texcolorTotal += texcolor;
-    //    }
-    //}
-    //texcolorTotal = texcolorTotal / 9;
-    //return float4(texcolorTotal.rgb, 1);
+    float4 texcolorTotal = 0;
+    for (int y = -1; y <= 1; y++)
+    {
+        for (int x = -1; x <= 1; x++)
+        {
+            float4 texcolor = tex1.Sample(smp, input.uv + float2(x / 640.0f, y / 360.f));
+            texcolorTotal += texcolor;
+        }
+    }
+    texcolorTotal = texcolorTotal / 9;
+   
     
     float4 colortex0 = tex0.Sample(smp, input.uv);
     float4 colortex1 = tex1.Sample(smp, input.uv);
 
-    float4 color = colortex0;
+    float4 color = 1 - colortex0;
     if (fmod(input.uv.y, 0.1f) < 0.05f)
     {
-        color = colortex1;
+        color = texcolorTotal;
     }
 
     return float4(color.rgb, 1);
+    
+    // postEffectの反転を中止
 }
 
 
